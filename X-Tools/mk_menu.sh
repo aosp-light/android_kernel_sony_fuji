dc9c83d7ccfcb22ecd04953ab415413b998f045e
paul-xxx <thepaulxxx@gmail.com>
2014-11-28 15:24:28 +0600

    add MK scripts

---
+++ X-Tools/mk_menu.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ../
+ARCH=arm CROSS_COMPILE=/home/paul/android/gcc/linaro/arm-cortex_a9-linux-gnueabihf-linaro_4.7.4-2014.06/bin/arm-cortex_a9-linux-gnueabihf- make mrproper
+cp fuji_hikari_defconfig .config
+ARCH=arm CROSS_COMPILE=/home/paul/android/gcc/linaro/arm-cortex_a9-linux-gnueabihf-linaro_4.7.4-2014.06/bin/arm-cortex_a9-linux-gnueabihf- make menuconfig
