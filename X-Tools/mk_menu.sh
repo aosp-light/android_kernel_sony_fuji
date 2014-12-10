98c71ad0271082f0dd3e5ca7b36b845c77db4962
paul-xxx <thepaulxxx@gmail.com>
2014-12-11 03:45:21 +0600

    hikari fix

--- X-Tools/mk_menu.sh
+++ X-Tools/mk_menu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ../
 ARCH=arm make mrproper
-cp arch/arm/configs/fuji_hikari_row_defconfig .config
+cp arch/arm/configs/fuji_hikari_defconfig .config
 #cp arch/arm/configs/fuji_aoba_defconfig .config
 ARCH=arm make menuconfig
