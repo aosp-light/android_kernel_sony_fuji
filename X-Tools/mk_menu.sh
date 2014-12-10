5a3e577853628895e5a5096f752fcb24dd4c7daf
paul-xxx <thepaulxxx@gmail.com>
2014-12-11 03:34:12 +0600

    vmlinux error fix

--- X-Tools/mk_menu.sh
+++ X-Tools/mk_menu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ../
-ARCH=arm make mrproper
+ARCH=arm CROSS_COMPILE=/home/paul/android/kernel/gcc/arm-eabi-4.7/bin/arm-eabi- make mrproper
 cp arch/arm/configs/fuji_hikari_row_defconfig .config
 #cp arch/arm/configs/fuji_aoba_defconfig .config
-ARCH=arm make menuconfig
+ARCH=arm CROSS_COMPILE=/home/paul/android/kernel/gcc/arm-eabi-4.7/bin/arm-eabi- make menuconfig
