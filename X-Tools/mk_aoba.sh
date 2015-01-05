4a878837dfb123f34a5ffa71519bde6e5f90080a
paul-xxx <thepaulxxx@gmail.com>
2015-01-05 10:01:07 +0600

    cleanup #3
    Conflicts:
    	X-Tools/mk_aoba.sh

--- X-Tools/mk_aoba.sh
+++ X-Tools/mk_aoba.sh
@@ -23,6 +23,7 @@ cp arch/arm/mach-msm/msm-buspm-dev.ko X-Tools/modules/msm-buspm-dev.ko
 cp block/test-iosched.ko X-Tools/modules/test-iosched.ko
 cp crypto/ansi_cprng.ko X-Tools/modules/ansi_cprng.ko
 cp drivers/crypto/msm/qce.ko X-Tools/modules/qce.ko
+<<<<<<< HEAD:X-Tools/mk_aoba.sh
 cp drivers/crypto/msm/qcedev.ko X-Tools/modules/qcedev.ko
 cp drivers/crypto/msm/qcrypto.ko X-Tools/modules/qcrypto.ko
 cp drivers/hid/hid-logitech-dj.ko X-Tools/modules/hid-logitech-dj.ko
@@ -39,3 +40,17 @@ cp net/sched/sch_dsmark.ko X-Tools/modules/sch_dsmark.ko
 
 echo "Done! Press ENTER for exit..."
 read && exit
+=======
+cp block/test-iosched.ko X-Tools/modules/test-iosched.ko
+cp arch/arm/mach-msm/msm-buspm-dev.ko X-Tools/modules/msm-buspm-dev.ko
+cp net/sched/sch_dsmark.ko X-Tools/modules/sch_dsmark.ko
+cp drivers/mmc/card/mmc_block_test.ko X-Tools/modules/mmc_block_test.ko
+cp drivers/media/video/gspca/gspca_main.ko X-Tools/modules/gspca_main.ko
+cp drivers/hid/hid-logitech-dj.ko X-Tools/modules/hid-logitech-d.ko
+cp net/sched/cls_flow.ko X-Tools/modules/cls_flow.ko
+cp crypto/ansi_cprng.ko X-Tools/modules/ansi_cprng.ko
+cp arch/arm/mach-msm/dal_remotetest.ko X-Tools/modules/dal_remotetest.ko
+cp arch/arm/mach-msm/dma_test.ko X-Tools/modules/dma_test.ko
+cp drivers/video/backlight/lcd.ko X-Tools/modules/lcd.ko
+cp drivers/scsi/scsi_wait_scan.ko X-Tools/modules/scsi_wait_scan.ko
+>>>>>>> 8909964... cleanup #3:X-Tools/cp_modules.sh
